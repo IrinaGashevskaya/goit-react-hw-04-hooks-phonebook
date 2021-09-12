3e22cf2ee6c643147f116dac9164146ec30c83af
IrinaGashevskaya <70062185+IrinaGashevskaya@users.noreply.github.com>
2021-09-12 13:58:13 +0300

    fix error

--- src/App.js
+++ src/App.js
@@ -7,16 +7,23 @@ import Filter from './components/Filter/Filter';
 import './App.css';
 import 'modern-normalize/modern-normalize.css';
 export default function App() {
-  const [contacts, setContacts] = useState([
+  const saved = localStorage.getItem('contacts');
+  let initialValue = [
     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  ];
+  if (saved) {
+    initialValue = JSON.parse(saved);
+  }
+  const [contacts, setContacts] = useState(initialValue);
   const [filter, setFilter] = useState('');
   useEffect(() => {
+    console.log(contacts);
     localStorage.setItem('contacts', JSON.stringify(contacts));
   }, [contacts]);
+
   const addContactSubmit = (name, number) => {
     if (contacts.map(contact => contact.name.toLowerCase()).includes(name.toLowerCase().trim())) {
       alert(`${name} is already in contacts `);
